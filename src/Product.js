892f55e0fc19863fb830fe79a15272c0840cb02d
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-08-18 09:47:54 +0100

    initial commit

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Product(props) {
+    return(
+        <div>
+            <h2>{props.product.name}</h2>
+            <p>{props.product.price.toLocaleString("en-US", { style: "currency", currency: "USD" })} - {props.product.description}</p>
+            <hr /> 
+        </div>
+    );
+}
+
+export default Product;
---
+++ src/vschoolProducts.js
@@ -0,0 +1,58 @@
+const vschoolProducts = [
+{
+    id: "1",
+    name: "Pencil",
+    price: 1.40,
+    description: "Perfect for those who can't remember things! 5/5 Highly recomended.",
+},
+{
+    id: "2",
+    name: "Books",
+    price: 3.80,
+    description: "Never stop learning!",
+},
+{
+   id: "3",
+   name: "Housing",
+   price: 0,
+   description: "Housing provided for out-of-state students or those who can commute.",
+},
+{
+    id: "4",
+    name: "Computer Rental",
+    price: 300,
+    description: "Don't have a computer! No problem.",
+},
+{
+    id: "5",
+    name: "Snacks",
+    price: 0,
+    description: "Free snacks.",
+},
+{
+    id: "6",
+    name: "Sticker Set",
+    price: 45.99,
+    description: "To prove to other devs that you know a lot.",
+},
+{
+    id: "7",
+    name: "Rubber Dukies",
+    price: 23.65,
+    description: "To help you solve your hardest coding problems.",
+},
+{
+    id: "8",
+    name: "Coffee",
+    price: 2.50,
+    description: "Wake Up!"
+},
+{
+    id: "9",
+    name: "Fidget Spinner",
+    price: 4.87,
+    description: "Because we like to pretend we are in high school.",
+}
+
+]
+export default vschoolProducts;
